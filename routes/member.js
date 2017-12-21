39894adbb7a794177c9bceb5d2580dd4ac24b0f4
salusn <salu.s@lightrains.com>
2017-12-21 12:06:33 +0530

    Added new router for member

---
+++ routes/member.js
@@ -0,0 +1,32 @@
+// jshint ignore: start
+var express = require('express');
+var router = express.Router();
+
+var Web3 = require('web3');
+var web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+
+const EscrowSale = require('../build/contracts/EscrowSale.json');
+const abi = EscrowSale.abi;
+const bytecode =EscrowSale.bytecode;
+
+web3.eth.defaultAccount = '0x69043fb55436609846cd209627d8662a67cc9e14';
+var CoursetroContract = new web3.eth.Contract(abi,'0x298e0c2a346519b836aebb04bf21181acbedec9e');
+
+console.log(CoursetroContract.methods.createEscrow)
+
+buyer = "0x6e87dd23faa98b6d7f4359830d9c19848a4ba491"
+seller = "0x7ef55c345a75cf0f6726c09aa559667ae6e883a6"
+arbitrator = "0x408678574c6cfb8675928d01b3b9f6f1ed249151"
+amount = "150"
+orderId = "7683"
+
+/* GET home page. */
+router.get('/member', function(req, res, next) {
+  res.render('member');
+});
+
+router.post('/member', function(req, res, next) {
+	console.log("kk")
+});
+
+module.exports = router;
